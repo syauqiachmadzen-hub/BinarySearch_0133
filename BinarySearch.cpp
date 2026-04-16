279b152c0af862ae51f5e8e7dde79ab97b5be845
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-04-16 10:22:06 +0700

    BinarySearch 3

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,3 +4,18 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+    }
+}
